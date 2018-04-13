4ac7e595568d417388c7132567734da212e23368
jwest91 <jwest91@hotmail.com>
2018-04-12 19:56:24 -0500

    Initial project build utilizing the asp.net framework and asp.net Identity.

---
+++ TCG App/TCG App/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TCG_App.MvcApplication" Language="C#" %>
---
+++ TCG App/TCG App/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TCG_App.Startup))]
+namespace TCG_App
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
