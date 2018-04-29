735459fb0163b850d43cc65516d6f4380879ec3e
Silok <Patrick.McGowan@trojans.dsu.edu>
2018-04-29 17:20:24 -0500

    Add community creation

---
+++ Models/CommunityViewModels.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace TCG_App.Models
+{
+    public class CommunityViewModels
+    {
+    }
+    
+    public class CreateCommunityViewModel
+    {
+        [Required]
+        [Display(Name ="Community Name")]
+        public string CommunityName { get; set; }
+
+        [Required]
+        [Display(Name = "Community Description")]
+        public string CommunityDesc { get; set; }
+
+        [Display(Name = "Community Location")]
+        public string CommunityLoc { get; set; }
+    }
+}
